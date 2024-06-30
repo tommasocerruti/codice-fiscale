e8319b99d3b3d27284a069b9828fb516d2137d7d
Tommaso Cerruti <tommaso.cerruti@casavo.com>
2024-06-30 20:17:34 +0200

    init

---
+++ lib/codice_fiscale.ex
@@ -0,0 +1,18 @@
+defmodule CodiceFiscale do
+  @moduledoc """
+  Documentation for `CodiceFiscale`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CodiceFiscale.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/codice_fiscale_test.exs
@@ -0,0 +1,8 @@
+defmodule CodiceFiscaleTest do
+  use ExUnit.Case
+  doctest CodiceFiscale
+
+  test "greets the world" do
+    assert CodiceFiscale.hello() == :world
+  end
+end
