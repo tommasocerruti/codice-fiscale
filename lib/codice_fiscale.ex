df33f9da0e12bd54a01d7b5d80ba385f36843de1
Tommaso Cerruti <tommaso.cerruti@casavo.com>
2024-07-05 23:20:07 +0200

    update: updating new main function

--- lib/codice_fiscale.ex
+++ lib/codice_fiscale.ex
@@ -1,37 +1,29 @@
 defmodule CodiceFiscale do
-  alias CodiceFiscale.{Conversion, CharCheck, Validation, DataLoader, StringProcessor}
+  alias CodiceFiscale.{Conversion, CharCheck, Validation, StringProcessor}
   require Logger
 
   @doc """
-  Inizializza il caricamento dei dati dei comuni.
-  """
-  def init do
-    DataLoader.load_comuni()
-  end
-
-  @doc """
-  Calcola il codice fiscale dato un nome, cognome, data di nascita, sesso e comune.
+  Calcola il codice fiscale dato un nome, cognome, data di nascita, sesso e codice ISTAT del luogo di nascita.
 
   ## Esempio
 
-      iex> CodiceFiscale.calcola("Mario", "Rossi", "1980-01-01", "M", "Roma(RM)")
+      iex> CodiceFiscale.calcola("Mario", "Rossi", "1980-01-01", "M", "H501")
       {:ok, "RSSMRA80A01H501X"}
 
   """
-  def calcola(nome, cognome, data_nascita, sesso, comune) do
+  def calcola(nome, cognome, data_nascita, sesso, codice) do
     Logger.info("Calcolo del codice fiscale per #{nome} #{cognome}")
     with :ok <- Validation.validate_nome(nome),
          :ok <- Validation.validate_cognome(cognome),
          :ok <- Validation.validate_data_nascita(data_nascita),
          :ok <- Validation.validate_sesso(sesso),
-         :ok <- Validation.validate_comune(comune) do
+         :ok <- Validation.validate_codice(codice) do
       cognome_processed = StringProcessor.process(cognome)
       nome_processed = StringProcessor.process(nome)
       codice_cognome = Conversion.cognome(cognome_processed)
       codice_nome = Conversion.nome(nome_processed)
       codice_data = Conversion.data_nascita(data_nascita, sesso)
-      codice_comune = Conversion.comune(comune)
-      parziale = "#{codice_cognome}#{codice_nome}#{codice_data}#{codice_comune}"
+      parziale = "#{codice_cognome}#{codice_nome}#{codice_data}#{codice}"
       carattere_controllo = CharCheck.calcola_carattere_controllo(parziale)
 
       {:ok, "#{parziale}#{carattere_controllo}"}
